13fc886c79d7b5e9a2dd51d51a0522bd8a910ac9
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-06-14 15:35:27 +0200

    toRoman(5) done

--- src/main/RomanNumerals.java
+++ src/main/RomanNumerals.java
@@ -3,6 +3,10 @@ package main;
 public class RomanNumerals {
 	public static String toRoman(int n) {
 		String retString = "";
+		if (n >= 5) {
+			retString += "V";
+			n -= 5;
+		}
 		for (int i = 0; i < n; i++) {
 			retString += "I";
 		}
