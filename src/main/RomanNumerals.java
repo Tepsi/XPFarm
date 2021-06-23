2caea0a6379d256931007f5e598e6ac16635c3d0
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-06-23 09:41:43 +0200

    Refactoring

--- src/main/RomanNumerals.java
+++ src/main/RomanNumerals.java
@@ -2,8 +2,8 @@ package main;
 
 public class RomanNumerals {
 
-	final static String[] ROMAN = { "M", "D", "C", "L", "X", "V", "I" };
-	final static int[] ARABIC = { 1000, 500, 100, 50, 10, 5, 1 };
+	final static String[] ROMAN = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+	final static int[] ARABIC = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
 
 	public static String toRoman(int n) {
 		String retString = "";
@@ -15,25 +15,9 @@ public class RomanNumerals {
 					retString += ROMAN[value];
 				}
 				n -= multiplier * ARABIC[value];
-			} else {
-				if (value < ARABIC.length - 1) {
-					if (value % 2 == 1) {
-						if (n >= ARABIC[value] - ARABIC[value + 1]) {
-							retString += ROMAN[value + 1] + ROMAN[value];
-							n -= (ARABIC[value] - ARABIC[value + 1]);
-						}
-					} else {
-						if (n >= ARABIC[value] - ARABIC[value + 2]) {
-							retString += ROMAN[value + 2] + ROMAN[value];
-							n -= (ARABIC[value] - ARABIC[value + 2]);
-						}
-					}
-				}
 			}
 		}
-
 		return retString;
-
 	}
 
 	public static int fromRoman(String romanNumeral) {
