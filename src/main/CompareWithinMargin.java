7c8ea7a55b870a20f3e10536002d2ae504602ee1
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-06-24 09:52:57 +0200

    Initial commit of Compare Within Margin

---
+++ src/main/CompareWithinMargin.java
@@ -0,0 +1,11 @@
+package main;
+
+public class CompareWithinMargin {
+	public static int closeCompare(double a, double b) {
+		return 0; // TODO
+	}
+
+	public static int closeCompare(double a, double b, double margin) {
+		return 0; // TODO
+	}
+}
---
+++ src/test/CompareWithinMarginTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CompareWithinMarginTest {
+
+	@Test
+	void testCloseCompareDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCloseCompareDoubleDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+}
