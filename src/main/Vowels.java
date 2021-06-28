96928941d31b4d2d55d3500441dd3c408294e57c
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-06-28 16:55:04 +0200

    final solution of Vowels

--- src/main/Vowels.java
+++ src/main/Vowels.java
@@ -2,8 +2,15 @@ package main;
 
 public class Vowels {
 	public static int getCount(String str) {
+		final String[] VOWELS = { "a", "e", "i", "o", "u" };
 		int vowelsCount = 0;
-		// your code here
+		for (String vowel : VOWELS) {
+			int current = str.indexOf(vowel);
+			while (current >= 0) {
+				vowelsCount++;
+				current = str.indexOf(vowel, current + 1);
+			}
+		}
 		return vowelsCount;
 	}
 }
--- src/test/VowelsTest.java
+++ src/test/VowelsTest.java
@@ -9,7 +9,13 @@ import main.Vowels;
 class VowelsTest {
 	@Test
 	public void testCase1() {
-//		assertEquals("Nope!", 5, Vowels.getCount("abracadabra"));
+		assertEquals(0, Vowels.getCount(""));
+		assertEquals(0, Vowels.getCount("x"));
+		assertEquals(1, Vowels.getCount("a"));
+		assertEquals(1, Vowels.getCount("ax"));
+		assertEquals(2, Vowels.getCount("aa"));
+		assertEquals(5, Vowels.getCount("aeiou"));
+		assertEquals(5, Vowels.getCount("abracadabra"));
 	}
 
 }
