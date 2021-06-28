c97ae7164ec2c0113d2a9dc6732f023449ba1f23
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-06-28 13:12:41 +0200

    Initial commit of Dinglemouse

---
+++ src/main/Dinglemouse.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Dinglemouse {
+	public static int[] paintLetterboxes(final int start, final int end) {
+	    // Your code here
+	    return new int[10];
+	  }
+
+}
---
+++ src/test/DinglemouseTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DinglemouseTest {
+
+	@Test
+	void testPaintLetterboxes() {
+		fail("Not yet implemented");
+	}
+
+}
