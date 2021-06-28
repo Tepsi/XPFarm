5c5012f2b1ef8d4066cf9223e1a4cb13fa7e74b2
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-06-28 13:29:31 +0200

    final solution Dinglemouse

--- src/main/Dinglemouse.java
+++ src/main/Dinglemouse.java
@@ -1,9 +1,18 @@
 package main;
 
+import java.util.Arrays;
+
 public class Dinglemouse {
 	public static int[] paintLetterboxes(final int start, final int end) {
-	    // Your code here
-	    return new int[10];
-	  }
+		int[] retArray = new int[10];
+		Arrays.fill(retArray, 0);
+		for (int i = start; i <= end; i++) {
+			String number = Integer.toString(i);
+			for (char c : number.toCharArray()) {
+				retArray[c - '0']++;
+			}
+		}
+		return retArray;
+	}
 
 }
--- src/test/DinglemouseTest.java
+++ src/test/DinglemouseTest.java
@@ -1,14 +1,23 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import org.junit.jupiter.api.Test;
+
+import main.Dinglemouse;
 
 class DinglemouseTest {
 
 	@Test
 	void testPaintLetterboxes() {
-		fail("Not yet implemented");
+		assertArrayEquals(new int[] { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 }, Dinglemouse.paintLetterboxes(1, 1));
+		assertArrayEquals(new int[] { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, Dinglemouse.paintLetterboxes(2, 2));
+		assertArrayEquals(new int[] { 0, 1, 1, 0, 0, 0, 0, 0, 0, 0 }, Dinglemouse.paintLetterboxes(1, 2));
+		assertArrayEquals(new int[] { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, Dinglemouse.paintLetterboxes(1, 9));
+		assertArrayEquals(new int[] { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 }, Dinglemouse.paintLetterboxes(10, 10));
+		assertArrayEquals(new int[] { 1, 3, 0, 0, 0, 0, 0, 0, 0, 0 }, Dinglemouse.paintLetterboxes(10, 11));
+
+		assertArrayEquals(new int[] { 1, 9, 6, 3, 0, 1, 1, 1, 1, 1 }, Dinglemouse.paintLetterboxes(125, 132));
 	}
 
 }
