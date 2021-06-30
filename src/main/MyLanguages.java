af70ac75b4aed3579929bb251c21420910c2fee1
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-06-30 16:35:18 +0200

    Initial commit of MyLanguages

---
+++ src/main/MyLanguages.java
@@ -0,0 +1,10 @@
+package main;
+
+import java.util.List;
+import java.util.Map;
+
+public class MyLanguages {
+  public static List<String> myLanguages(final Map<String, Integer> results) {
+    return null;
+  }
+}
