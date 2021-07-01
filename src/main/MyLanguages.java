31dbe52cc21a68e7de6bebffbb648480008f2d22
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-07-01 08:05:52 +0200

    Final solution of MyLanguages

--- src/main/MyLanguages.java
+++ src/main/MyLanguages.java
@@ -1,10 +1,25 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class MyLanguages {
-  public static List<String> myLanguages(final Map<String, Integer> results) {
-    return null;
-  }
+	public static List<String> myLanguages(final Map<String, Integer> results) {
+		List<String> retList = new ArrayList<>();
+		int maxValue = 59;
+		for (Map.Entry<String, Integer> result : results.entrySet()) {
+			if (result.getValue() > maxValue) {
+				maxValue = result.getValue();
+			}
+		}
+		for (int i = maxValue; i >= 60; i--) {
+			for (Map.Entry<String, Integer> result : results.entrySet()) {
+				if (result.getValue() == i) {
+					retList.add(result.getKey());
+				}
+			}
+		}
+		return retList;
+	}
 }
--- src/test/MyLanguagesTest.java
+++ src/test/MyLanguagesTest.java
@@ -1,14 +1,49 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
+
+import main.MyLanguages;
 
 class MyLanguagesTest {
 
 	@Test
-	void testMyLanguages() {
-		fail("Not yet implemented");
-	}
+	public void basicTests() {
+		final Map<String, Integer> mapEmpty = new HashMap<>();
+		assertEquals(Collections.emptyList(), MyLanguages.myLanguages(mapEmpty));
 
+		final Map<String, Integer> mapOneSmall = new HashMap<>();
+		mapOneSmall.put("Java", 10);
+		assertEquals(Collections.emptyList(), MyLanguages.myLanguages(mapOneSmall));
+
+		final Map<String, Integer> mapTwoDiff = new HashMap<>();
+		mapTwoDiff.put("Java", 10);
+		mapTwoDiff.put("Ruby", 60);
+		assertEquals(Arrays.asList("Ruby"), MyLanguages.myLanguages(mapTwoDiff));
+
+		final Map<String, Integer> map1 = new HashMap<>();
+		map1.put("Java", 10);
+		map1.put("Ruby", 80);
+		map1.put("Python", 65);
+		assertEquals(Arrays.asList("Ruby", "Python"), MyLanguages.myLanguages(map1));
+	
+		final Map<String, Integer> map2 = new HashMap<>();
+		map2.put("Hindi", 60);
+		map2.put("Dutch", 93);
+		map2.put("Greek", 71);
+		assertEquals(Arrays.asList("Dutch", "Greek", "Hindi"), MyLanguages.myLanguages(map2));
+		
+		final Map<String, Integer> map3 = new HashMap<>();
+		map3.put("C++", 50);
+		map3.put("ASM", 10);
+		map3.put("Haskell", 20);
+		assertEquals(Collections.emptyList(), MyLanguages.myLanguages(map3));
+		
+	}
 }
