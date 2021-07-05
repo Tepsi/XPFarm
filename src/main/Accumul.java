d27c7fa9be23d2921dbcdff608da359f6fc791a5
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-07-05 14:36:45 +0200

    Initial commit of Accumul

---
+++ src/main/Accumul.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Accumul {
+    
+    public static String accum(String s) {
+     // your code
+    }
+}
---
+++ src/test/AccumulTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AccumulTest {
+
+	@Test
+	void testAccum() {
+		fail("Not yet implemented");
+	}
+
+}
