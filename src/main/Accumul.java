e27dbe8fd372705a996fb17aaedae5beeb1369bf
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-07-05 14:57:02 +0200

    Final commit of Accumul

--- src/main/Accumul.java
+++ src/main/Accumul.java
@@ -1,8 +1,23 @@
 package main;
 
 public class Accumul {
-    
-    public static String accum(String s) {
-     // your code
-    }
+
+	public static String accum(String s) {
+		String retString = "";
+		for (int i = 0; i < s.length(); i++) {
+			if (!retString.isEmpty()) {
+				retString += "-";
+			}
+			retString += add(s.substring(i, i + 1), i);
+		}
+		return retString;
+	}
+
+	private static String add(String letter, int count) {
+		String retString = letter.toUpperCase();
+		for (int i = 1; i <= count; i++) {
+			retString += letter.toLowerCase();
+		}
+		return retString;
+	}
 }
--- src/test/AccumulTest.java
+++ src/test/AccumulTest.java
@@ -1,14 +1,37 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import main.Accumul;
+
 class AccumulTest {
 
-	@Test
-	void testAccum() {
-		fail("Not yet implemented");
-	}
+    private static void testing(String actual, String expected) {
+        assertEquals(expected, actual);
+    }
+
+    
+    @Test
+    public void testZoli() {
+        testing(Accumul.accum(""), "");
+        testing(Accumul.accum("A"), "A");
+        testing(Accumul.accum("a"), "A");
+        testing(Accumul.accum("cd"), "C-Dd");
+        testing(Accumul.accum("QhUz"), "Q-Hh-Uuu-Zzzz");
+    }
+
+    
+    @Test
+    public void test() {
+        System.out.println("Fixed Tests accum");
+        testing(Accumul.accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+        testing(Accumul.accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+        testing(Accumul.accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+        testing(Accumul.accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+        testing(Accumul.accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+    }
+    
 
 }
