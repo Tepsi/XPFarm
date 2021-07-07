a58d37bd919082b1ed71ca6ff18e1bc2eb376a7b
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-07-07 15:14:11 +0200

    Final solution of SquareDigit

--- src/main/SquareDigit.java
+++ src/main/SquareDigit.java
@@ -2,8 +2,22 @@ package main;
 
 public class SquareDigit {
 
-	  public int squareDigits(int n) {
-	    // TODO Implement me
-	  }
+	public int squareDigits(int n) {
+		String strOut = "";
+		String strIn = Integer.toString(n);
 
+		strIn = strIn.replace("-", "");
+
+		for (int i = 0; i < strIn.length(); i++) {
+			strOut += square(strIn.substring(i, i + 1));
+		}
+
+		return Integer.parseInt(strOut);
 	}
+
+	private String square(String digit) {
+		int number = Integer.parseInt(digit);
+		return Integer.toString(number * number);
+	}
+
+}
--- src/test/SquareDigitTest.java
+++ src/test/SquareDigitTest.java
@@ -1,14 +1,22 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+
+import main.SquareDigit;
 
 class SquareDigitTest {
 
 	@Test
-	void testSquareDigits() {
-		fail("Not yet implemented");
+	public void test() {
+		assertEquals(0, new SquareDigit().squareDigits(0));
+		assertEquals(1, new SquareDigit().squareDigits(1));
+		assertEquals(1, new SquareDigit().squareDigits(-1));
+		assertEquals(4, new SquareDigit().squareDigits(2));
+		assertEquals(10, new SquareDigit().squareDigits(10));
+		assertEquals(49, new SquareDigit().squareDigits(23));
+		assertEquals(2536, new SquareDigit().squareDigits(56));
+		assertEquals(811181, new SquareDigit().squareDigits(9119));
 	}
-
 }
