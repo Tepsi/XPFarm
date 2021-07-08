c4ec699e6a82da09aeb582c8d1fa0b567aa492c7
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-07-08 13:13:44 +0200

    Initial commit of Descending Order

---
+++ src/test/DescendingOrderTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import main.DescendingOrder;
+
+
+public class DescendingOrderTest {
+
+    @Test
+    public void test_01() {
+        assertEquals(0, DescendingOrder.sortDesc(0));
+    }    
+
+    @Test
+    public void test_02() {
+        assertEquals(51, DescendingOrder.sortDesc(15));
+    }
+    
+
+    @Test
+    public void test_03() {
+        assertEquals(987654321, DescendingOrder.sortDesc(123456789));
+    }
+}
