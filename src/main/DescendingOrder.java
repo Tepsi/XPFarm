7186daa046f8f22654aa3dfa349be134b7901fd3
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-07-08 13:39:48 +0200

    Final commit of DescendingOrder

--- src/main/DescendingOrder.java
+++ src/main/DescendingOrder.java
@@ -1,7 +1,29 @@
 package main;
 
 public class DescendingOrder {
-	  public static int sortDesc(final int num) {
-	    return 0;
-	  }
+	public static int sortDesc(final int num) {
+		return calculate(buildStatistic(num));
 	}
+	
+	private static int calculate(int[] statistic) {
+		int retNum = 0;
+		for (int i = 9; i >= 0; i--) {
+			for (int j = 0; j < statistic[i]; j++) {
+				retNum = retNum * 10 + i;
+			}
+		}
+		return retNum;
+	}
+
+	private static int[] buildStatistic(int number) {
+		int[] statistic = new int[10];
+		String numStr = Integer.toString(number);
+
+		for (int i = 0; i < numStr.length(); i++) {
+			String digit = numStr.substring(i, i + 1);
+			int numDigit = Integer.parseInt(digit);
+			statistic[numDigit]++;
+		}
+		return statistic;
+	}
+}
--- src/test/DescendingOrderTest.java
+++ src/test/DescendingOrderTest.java
@@ -11,17 +11,16 @@ public class DescendingOrderTest {
 
     @Test
     public void test_01() {
-        assertEquals(0, DescendingOrder.sortDesc(0));
-    }    
+        assertEquals("Zero should return 0", 0, DescendingOrder.sortDesc(0) );
+        assertEquals("Single digit should return the same digit", 5, DescendingOrder.sortDesc(5));
+        assertEquals("Double digits of the same should return the same number", 11, DescendingOrder.sortDesc(11));
+        assertEquals("Double digits should be ordered", 21, DescendingOrder.sortDesc(12));
+        assertEquals("Multi digits should be ordered", 531, DescendingOrder.sortDesc(315));
+        assertEquals("Multi digits should be ordered", 2211, DescendingOrder.sortDesc(2112));
+        assertEquals("Multi digits should be ordered", 7665441, DescendingOrder.sortDesc(4765416));
+    }   
 
-    @Test
-    public void test_02() {
-        assertEquals(51, DescendingOrder.sortDesc(15));
-    }
+
     
 
-    @Test
-    public void test_03() {
-        assertEquals(987654321, DescendingOrder.sortDesc(123456789));
-    }
 }
