27801bf0e007c83f77cd53846ac56c6a3dcf5b1d
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-07-12 12:53:17 +0200

    Display Name test

--- src/test/DRootTest.java
+++ src/test/DRootTest.java
@@ -3,13 +3,15 @@ package test;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 import main.DRoot;
 
 public class DRootTest {
     @Test
+    @DisplayName("Test Zoli")
     public void Test1() {
-      assertEquals( "Zero should return zero" , 0, DRoot.digital_root(0));
+      assertEquals( "Zero should return zero" , 1, DRoot.digital_root(0));
       assertEquals( "Any one digit number should return itself" , 1, DRoot.digital_root(1));
       assertEquals( "Two digits should be added together" , 3, DRoot.digital_root(12));
       assertEquals( "Should always return only one digit" , 2, DRoot.digital_root(56));
