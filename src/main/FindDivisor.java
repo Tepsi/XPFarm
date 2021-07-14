bb96961098f5ad9aa74121ad97a7776d67a3bbfe
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-07-14 15:38:23 +0200

    Final commit of Find Divisol

--- src/main/FindDivisor.java
+++ src/main/FindDivisor.java
@@ -2,9 +2,17 @@ package main;
 
 public class FindDivisor {
 
-	  public long numberOfDivisors(int n) {
-	    // TODO please write your code below this comment
-
-	  }
-
+	public long numberOfDivisors(int n) {
+		long retNum = 0;
+		double root = Math.sqrt(n);
+		for (double i = 1; i < root; i++) {
+			if (n % i == 0) {
+				retNum+= 2;
+			}
+		}
+		if (n % root == 0) {
+			retNum++;
+		}
+		return retNum;
 	}
+}
--- src/test/FindDivisorTest.java
+++ src/test/FindDivisorTest.java
@@ -11,22 +11,14 @@ class FindDivisorTest {
 	FindDivisor fd = new FindDivisor();
 
 	@Test
-	public void fourTest() {
-		assertEquals("Sould return 3 if the parameter equals 4", 3, fd.numberOfDivisors(4));
+	public void Test() {
+		assertEquals("Should return 1 if the parameter equals 1", 1, fd.numberOfDivisors(1));
+		assertEquals("Should return 2 if the parameter equals 2", 2, fd.numberOfDivisors(2));
+		assertEquals("Should return 2 if the parameter equals 3", 2, fd.numberOfDivisors(3));
+		assertEquals("Should return 2 if the parameter equals 5", 2, fd.numberOfDivisors(5));
+		assertEquals("Should return 2 if the parameter equals 5", 2, fd.numberOfDivisors(5));
+		assertEquals("Should return 6 if the parameter equals 12", 6, fd.numberOfDivisors(12));
+		assertEquals("Should return 8 if the parameter equals 30", 8, fd.numberOfDivisors(30));
 	}
   
-	@Test
-	public void fiveTest() {
-		assertEquals("Sould return 2 if the parameter equals 5", 2, fd.numberOfDivisors(5));
-	}
-  
-	@Test
-	public void twelveTest() {
-		assertEquals("Sould return 6 if the parameter equals 12", 6, fd.numberOfDivisors(12));
-	}
-  
-	@Test
-	public void thirtyTest() {
-		assertEquals("Sould return 8 if the parameter equals 30", 8, fd.numberOfDivisors(30));
-	}
 }
