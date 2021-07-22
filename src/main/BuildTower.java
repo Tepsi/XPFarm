16b29d0809b3e324f4283ca5e8b8dace4bbab54e
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-07-22 16:28:44 +0200

    Final commit of Build Tower

--- src/main/BuildTower.java
+++ src/main/BuildTower.java
@@ -1,10 +1,40 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BuildTower {
-	
+
 	public static String[] buildTower(int level) {
-		String[] retStringArray = {""};
-		return retStringArray;
+		List<String> lista = new ArrayList<>();
+
+		String szint = buildFirstLevel(level);
+		lista.add(szint);
+
+		for (int i = 1; i < level; i++) {
+			szint = nextLevel(szint);
+			lista.add(szint);
+		}
+		
+		return (String[]) lista.toArray(new String[level]);
+	}
+
+	private static String buildFirstLevel(int level) {
+		String szint = "";
+		for (int i = 1; i <= level * 2 - 1; i++) {
+			if (i == level) {
+				szint += "*";
+			} else {
+				szint += " ";
+			}
+		}
+		return szint;
+	}
+
+	private static String nextLevel(String szint) {
+		szint = szint.replace(" *", "**");
+		szint = szint.replace("* ", "**");
+		return szint;
 	}
 
 }
--- src/test/BuildTowerTest.java
+++ src/test/BuildTowerTest.java
@@ -1,14 +1,21 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
+
+import main.BuildTower;
 
 class BuildTowerTest {
 
 	@Test
 	void testBuildTower() {
-		fail("Not yet implemented");
+		assertTrue(Arrays.equals(new String[] { "*" }, BuildTower.buildTower(1)), "Level 1 tower is just one *");
+		assertTrue(Arrays.equals(new String[] { " * ", "***" }, BuildTower.buildTower(2)), "Level 2 tower is just two levels");
+		assertTrue(Arrays.equals(new String[] { "  *  ", " *** ", "*****" }, BuildTower.buildTower(3)), "Level 3 tower has 3 levels");
+
 	}
 
 }
