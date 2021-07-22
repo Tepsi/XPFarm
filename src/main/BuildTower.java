d9c590dc9535b7f3ba13686417cb437539ae5d61
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-07-22 15:53:19 +0200

    Initial commit of Build Tower

---
+++ src/main/BuildTower.java
@@ -0,0 +1,10 @@
+package main;
+
+public class BuildTower {
+	
+	public static String[] buildTower(int level) {
+		String[] retStringArray = {""};
+		return retStringArray;
+	}
+
+}
---
+++ src/test/BuildTowerTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BuildTowerTest {
+
+	@Test
+	void testBuildTower() {
+		fail("Not yet implemented");
+	}
+
+}
