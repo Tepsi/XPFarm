f300ee375523dd19af1de3dad76556609d5e8777
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-07-22 13:55:47 +0200

    Final commit of Tv remote

--- src/main/TvRemote.java
+++ src/main/TvRemote.java
@@ -1,9 +1,37 @@
 package main;
 
 public class TvRemote {
+
 	public static int tvRemote(final String word) {
-		// Your code here
-		return 1;
+
+		int posX = 0;
+		int posY = 0;
+		int steps = 0;
+		for (int i = 0; i < word.length(); i++) {
+			int[] coord = findButton(word.charAt(i));
+			steps += Math.abs(coord[0] - posX) + Math.abs(coord[1] - posY) + 1;
+			posX = coord[0];
+			posY = coord[1];
+		}
+		return steps;
+
+	}
+
+	private static int[] findButton(char letter) {
+		char[][] KEYBOARD = { { 'a', 'b', 'c', 'd', 'e', '1', '2', '3' }, { 'f', 'g', 'h', 'i', 'j', '4', '5', '6' },
+				{ 'k', 'l', 'm', 'n', 'o', '7', '8', '9' }, { 'p', 'q', 'r', 's', 't', '.', '@', '0' },
+				{ 'u', 'v', 'w', 'x', 'y', 'z', '_', '/' } };
+		int[] coord = { 0, 0 };
+		for (int i = 0; i < KEYBOARD.length; i++) {
+			for (int j = 0; j < KEYBOARD[i].length; j++) {
+				if (KEYBOARD[i][j] == letter) {
+					coord[0] = i;
+					coord[1] = j;
+					return coord;
+				}
+			}
+		}
+		return coord;
 	}
 
 }
--- src/test/TvRemoteTest.java
+++ src/test/TvRemoteTest.java
@@ -1,14 +1,24 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+
+import main.TvRemote;
 
 class TvRemoteTest {
 
 	@Test
 	void testTvRemote() {
-		fail("Not yet implemented");
+		assertEquals(1, TvRemote.tvRemote("a"), "A is just one OK");
+		assertEquals(2, TvRemote.tvRemote("b"), "B should step 1 and OK");
+		assertEquals(2, TvRemote.tvRemote("f"), "F should step down and OK");
+		assertEquals(3, TvRemote.tvRemote("c"), "Double-step for C and OK");
+		assertEquals(12, TvRemote.tvRemote("/"), "Lots of steps and OK");
+		assertEquals(3, TvRemote.tvRemote("ab"), "Multiletter combinations should add up");
+		assertEquals(3, TvRemote.tvRemote("af"), "Multiletter combinations should add up");
+		assertEquals(16, TvRemote.tvRemote("does"), "Multiletter words should work as well");
+		assertEquals(36, TvRemote.tvRemote("codewars"), "Multiletter words should work as well");
 	}
 
 }
