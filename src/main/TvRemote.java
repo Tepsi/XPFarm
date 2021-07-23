c5c3f77364c29b88831286037f8d7940566e5d9c
Zoltán Kuttner <kuttner.zoltan@gmail.com>
2021-07-23 15:34:16 +0200

    Stuck with Tv Remote

--- src/main/TvRemote.java
+++ src/main/TvRemote.java
@@ -4,34 +4,111 @@ public class TvRemote {
 
 	public static int tvRemote(final String word) {
 
-		int posX = 0;
-		int posY = 0;
-		int steps = 0;
+		RemoteState remoteState = new RemoteState();
 		for (int i = 0; i < word.length(); i++) {
-			int[] coord = findButton(word.charAt(i));
-			steps += Math.abs(coord[0] - posX) + Math.abs(coord[1] - posY) + 1;
-			posX = coord[0];
-			posY = coord[1];
+			char currentLetter = word.charAt(i);
+			if (needsShift(word, i, remoteState.isCapital())) {
+				remoteState = pressButton(remoteState, "Shift");
+				remoteState.toggleCapital();
+			}
+			remoteState = pressButton(remoteState, Character.toString(currentLetter).toLowerCase());
+			if (remoteState.getSteps() == -1) {
+				return -1;
+			}
 		}
-		return steps;
-
+		return remoteState.getSteps();
 	}
 
-	private static int[] findButton(char letter) {
-		char[][] KEYBOARD = { { 'a', 'b', 'c', 'd', 'e', '1', '2', '3' }, { 'f', 'g', 'h', 'i', 'j', '4', '5', '6' },
-				{ 'k', 'l', 'm', 'n', 'o', '7', '8', '9' }, { 'p', 'q', 'r', 's', 't', '.', '@', '0' },
-				{ 'u', 'v', 'w', 'x', 'y', 'z', '_', '/' } };
-		int[] coord = { 0, 0 };
-		for (int i = 0; i < KEYBOARD.length; i++) {
-			for (int j = 0; j < KEYBOARD[i].length; j++) {
-				if (KEYBOARD[i][j] == letter) {
-					coord[0] = i;
-					coord[1] = j;
-					return coord;
+	private static RemoteState pressButton(RemoteState remoteState, String letter) {
+		RemoteState newRemoteState = findButton(letter);
+		if (newRemoteState.getSteps() == -1) {
+			return newRemoteState;
+		}
+		int step = Math.abs(newRemoteState.getPosX() - remoteState.getPosX())
+				+ Math.abs(newRemoteState.getPosY() - remoteState.getPosY()) + 1;
+		newRemoteState.setSteps(remoteState.getSteps() + step);
+		newRemoteState.setCapital(remoteState.isCapital());
+		return newRemoteState;
+	}
+
+	private static boolean needsShift(String word, int currentPos, boolean capital) {
+		for (int i = currentPos; i < word.length(); i++) {
+			char letter = word.charAt(i);
+			if (Character.isAlphabetic(letter)) {
+				return (Character.isUpperCase(letter) && !capital) || (Character.isLowerCase(letter) && capital);
+			}
+		}
+		return false;
+	}
+
+	private static RemoteState findButton(String letter) {
+		String[][] KEYBOARD = { { "a", "b", "c", "d", "e", "1", "2", "3" }, { "f", "g", "h", "i", "j", "4", "5", "6" },
+				{ "k", "l", "m", "n", "o", "7", "8", "9" }, { "p", "q", "r", "s", "t", ".", "@", "0" },
+				{ "u", "v", "w", "x", "y", "z", "_", "/" }, { "Shift", " ", "", "", "", "", "", "" } };
+
+		RemoteState remoteState = new RemoteState();
+		for (int x = 0; x < KEYBOARD.length; x++) {
+			for (int y = 0; y < KEYBOARD[x].length; y++) {
+				if (KEYBOARD[x][y].equals(letter)) {
+					remoteState.setPosX(x);
+					remoteState.setPosY(y);
+					return remoteState;
 				}
 			}
 		}
-		return coord;
+		remoteState.setSteps(-1);
+		return remoteState;
+	}
+
+	public static class RemoteState {
+		int posX;
+		int posY;
+		int steps;
+		boolean capital;
+
+		public RemoteState() {
+			posX = 0;
+			posY = 0;
+			steps = 0;
+			capital = false;
+		}
+
+		public int getPosX() {
+			return posX;
+		}
+
+		public void setPosX(int posX) {
+			this.posX = posX;
+		}
+
+		public int getPosY() {
+			return posY;
+		}
+
+		public void setPosY(int posY) {
+			this.posY = posY;
+		}
+
+		public boolean isCapital() {
+			return capital;
+		}
+
+		public void setCapital(boolean capital) {
+			this.capital = capital;
+		}
+
+		public void toggleCapital() {
+			capital = !capital;
+		}
+
+		public void setSteps(int steps) {
+			this.steps = steps;
+		}
+
+		public int getSteps() {
+			return steps;
+		}
+
 	}
 
 }
--- src/test/TvRemoteTest.java
+++ src/test/TvRemoteTest.java
@@ -9,16 +9,55 @@ import main.TvRemote;
 class TvRemoteTest {
 
 	@Test
+	void testTvRemoteRandom() {
+//		assertEquals(278, TvRemote.tvRemote("xgBvUnTmy3f3 J3jbSlt7MJzLmJJFL"), "random test");
+	}
+
+	@Test
 	void testTvRemote() {
-		assertEquals(1, TvRemote.tvRemote("a"), "A is just one OK");
-		assertEquals(2, TvRemote.tvRemote("b"), "B should step 1 and OK");
-		assertEquals(2, TvRemote.tvRemote("f"), "F should step down and OK");
-		assertEquals(3, TvRemote.tvRemote("c"), "Double-step for C and OK");
+
+		assertEquals(1, TvRemote.tvRemote("a"), "a is just one OK");
+		assertEquals(2, TvRemote.tvRemote("b"), "b should step 1 and OK");
+		assertEquals(2, TvRemote.tvRemote("f"), "f should step down and OK");
+		assertEquals(3, TvRemote.tvRemote("c"), "Double-step for c and OK");
 		assertEquals(12, TvRemote.tvRemote("/"), "Lots of steps and OK");
 		assertEquals(3, TvRemote.tvRemote("ab"), "Multiletter combinations should add up");
 		assertEquals(3, TvRemote.tvRemote("af"), "Multiletter combinations should add up");
 		assertEquals(16, TvRemote.tvRemote("does"), "Multiletter words should work as well");
 		assertEquals(36, TvRemote.tvRemote("codewars"), "Multiletter words should work as well");
+		assertEquals(12, TvRemote.tvRemote("A"), "Capital letters need Shift");
+		assertEquals(16, TvRemote.tvRemote("a A"), "Mix of capital and noncapital letters");
+		assertEquals(15, TvRemote.tvRemote("a Z"), "Should optimize pressing shift");
+		assertEquals(12, TvRemote.tvRemote("z."), "Capital and non-alpha tests");
+		assertEquals(15, TvRemote.tvRemote("Z."), "Capital and non-alpha tests");
+		assertEquals(21, TvRemote.tvRemote("a 1E"), "Should optimize pressing shift");
+		assertEquals(31, TvRemote.tvRemote("F_m"), "Should optimize pressing shift");
+		assertEquals(-1, TvRemote.tvRemote("%"), "Wrong button entered");
+		assertEquals(22, TvRemote.tvRemote("t_U"), "Should optimize pressing shift");
+		assertEquals(22, TvRemote.tvRemote("Af_m"), "Should optimize pressing shift");
+		
+	}
+
+	@Test
+	public void upper() {
+		assertEquals(27, TvRemote.tvRemote("DOES"));
+		assertEquals(26, TvRemote.tvRemote("YOUR"));
+		assertEquals(38, TvRemote.tvRemote("SOLUTION"));
+		assertEquals(23, TvRemote.tvRemote("WORK"));
+		assertEquals(21, TvRemote.tvRemote("FOR"));
+		assertEquals(32, TvRemote.tvRemote("THESE"));
+		assertEquals(28, TvRemote.tvRemote("WORDS"));
+	}
+
+	@Test
+	public void mixed() {
+		assertEquals(40, TvRemote.tvRemote("Does"));
+		assertEquals(37, TvRemote.tvRemote("Your"));
+		assertEquals(49, TvRemote.tvRemote("Solution"));
+		assertEquals(30, TvRemote.tvRemote("Work"));
+		assertEquals(28, TvRemote.tvRemote("For"));
+		assertEquals(41, TvRemote.tvRemote("These"));
+		assertEquals(35, TvRemote.tvRemote("Words"));
 	}
 
 }
